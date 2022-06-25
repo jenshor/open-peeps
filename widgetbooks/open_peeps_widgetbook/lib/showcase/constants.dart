51d1206679ece9aabc148856ae8b77ba5210ca0c
Jens Horstmann <jens@widgetbook.io>
2022-06-25 14:51:09 +0200

    docs(open_peeps): :art: add open peeps banner image

---
+++ widgetbooks/open_peeps_widgetbook/lib/showcase/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const List<Color> colors = [
+  Color(0xFF99d3a8),
+  Color(0xFFa9dbda),
+  Color(0xFFe6c7f3),
+  Color(0xFFfacca9),
+  Color(0xFFf0a6ab)
+];
